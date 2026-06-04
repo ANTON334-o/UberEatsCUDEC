793d38c5475c4ce2644067049c03e795aa2728e7
ANTON334-o <eliseogamer64@gmail.com>
2026-06-03 22:05:33 -0600

    tercera parte

---
+++ js/db.js
@@ -0,0 +1,8 @@
+db.collection("platillos").onSnapshot((datos)=>{
+   datos.forEach((registro) => {
+    console.log(registro);
+    
+   });
+
+
+});
--- js/index.js
+++ js/index.js
@@ -1,13 +1,18 @@
-btnAgregarPlatillo = document.getElementById('btnAgrearPlatillo');
 document.addEventListener('DOMContentLoaded', function() {
+
   // nav menu
   const menus = document.querySelectorAll('.side-menu');
-  M.Sidenav.init(menus, {edge: 'right'});
-  // add recipe form
+  M.Sidenav.init(menus, { edge: 'right' });
+
+  // formulario
   const forms = document.querySelectorAll('.side-form');
-  M.Sidenav.init(forms, {edge: 'left'});
-});
-btnAgregarPlatillo.addEventListener('click', function() {
-  alert('Platillo agregado');
+  M.Sidenav.init(forms, { edge: 'left' });
+
+  const btnAgregarPlatillo =
+    document.getElementById('btnAgregarplatillo');
+
+  btnAgregarPlatillo.addEventListener('click', function() {
+    alert('Platillo agregado');
+  });
 
 });
