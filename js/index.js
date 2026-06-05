6455fb105e5b0b1bf9bc02bf164c286dcb170561
ANTON334-o <eliseogamer64@gmail.com>
2026-06-04 21:21:20 -0600

    Tercer avance

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+let contenido = "";
 document.addEventListener('DOMContentLoaded', function() {
 
   // nav menu
@@ -16,3 +17,29 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
 });
+
+function mostrarplatillo(platillo,  id) {
+   contenido += 
+   `<div class="card-panel recipe white row" data-id="${id}">
+   <div class="recipe-details">
+   <div class="recipe-title">
+   ${platillo.nombre}
+   </div>
+  <div class="recipe-ingredients">
+     ${platillo.ingredientes}
+  </div>
+  
+   <div class="recipe-title">
+   Precio: $${platillo.precio}
+  </div>
+  </div>
+  <div class="recipe-delete">
+  <i class="material-icons" data-id="${id}"
+  >delete_outline</i>
+  </div>
+  </div>`;
+
+
+  document.querySelector('.recipes').innerHTML = contenido;
+}
+
