8feaa2dd08bf796b4a6db54fcf753f4f0b9e72dc
ANTON334-o <79879831+ANTON334-o@users.noreply.github.com>
2026-07-25 22:44:56 -0600

    Delete UberEatsCUDEC-arreglado directory

--- UberEatsCUDEC-arreglado/js/sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('sw-cache').then(function(cache) {
-        return cache.add('index.html');
-    })
-  );
-});
