5bca70cef9c6c4ea478908e2fdc4f365ac2cbe61
ANTON334-o <79879831+ANTON334-o@users.noreply.github.com>
2026-07-25 22:44:25 -0600

    Add files via upload

---
+++ UberEatsCUDEC-arreglado/js/firebase.js
@@ -0,0 +1,19 @@
+
+
+ 
+  const firebaseConfig = {
+    apiKey: "AIzaSyBTKwYVY_SYmBAzCX0pnp1JFu0cJHrV_kI",
+    authDomain: "ubereatscudeceliseo.firebaseapp.com",
+    projectId: "ubereatscudeceliseo",
+    storageBucket: "ubereatscudeceliseo.firebasestorage.app",
+    messagingSenderId: "657099359169",
+    appId: "1:657099359169:web:bc5f9216575a091aa42540",
+    measurementId: "G-0B02KL6DDG"
+  };
+
+  
+
+
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
+
