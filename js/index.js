e4831f96ab0847687eecfed6f7622825f8992a76
ANTON334-o <eliseogamer64@gmail.com>
2026-08-06 21:00:03 -0600

    correcio

--- js/index.js
+++ js/index.js
@@ -1,47 +1,56 @@
-const btnAgregarPlatillo = document.getElementById('btnAgregarPlatillo');
 let contenido = "";
-
 document.addEventListener('DOMContentLoaded', function() {
+
   // nav menu
   const menus = document.querySelectorAll('.side-menu');
-  M.Sidenav.init(menus, {edge: 'right'});
-  // add recipe form
+  M.Sidenav.init(menus, { edge: 'right' });
+
+  // formulario
   const forms = document.querySelectorAll('.side-form');
-  M.Sidenav.init(forms, {edge: 'left'});
+  M.Sidenav.init(forms, { edge: 'left' });
+
+  const btnAgregarPlatillo =
+    document.getElementById('btnAgregarplatillo');
+
+  btnAgregarPlatillo.addEventListener('click', function() {
+    alert('Platillo agregado');
+  });
+
 });
 
-btnAgregarPlatillo.addEventListener('click', function() {
-  alert('Platillo agregado');
-});
-
-function mostrarPlatillo(platillo, id){
-  contenido +=
-  `<div class="card-panel recipe white row" id="${id}">
-    <div class= "recipe-details">
-      <div class= "recipe-title">
-         Nombre: ${platillo.nombre}
-      </div>
-      <div class="recipe-ingredients">
-  Ingredientes: ${platillo.ingredientes}
-      </div>
-      <div class="recipe-price">
-  Precio: $${platillo.precio}
-      </div>
-    </div>
-      <div class="recipe-delete">
-        <i class="material-icons" data-id="${id}">delete_outline</i>
-      </div>
-
+function mostrarplatillo(platillo,  id) {
+   contenido += 
+   `<div class="card-panel recipe white row" data-id="${id}">
+   <div class="recipe-details">
+   <div class="recipe-title">
+   ${platillo.nombre}
+   </div>
+  <div class="recipe-ingredients">
+     ${platillo.ingredientes}
+  </div>
+  
+   <div class="recipe-title">
+   Precio: $${platillo.precio}
+  </div>
+  </div>
+  <div class="recipe-delete">
+  <i class="material-icons" data-id="${id}"
+  >delete_outline</i>
+  </div>
   </div>`;
+
+
   document.querySelector('.recipes').innerHTML = contenido;
 }
 
-function actualizarPlatillo(platillo, id){
+function actualizarplatillo(platillo, id) {
   let tarjeta = document.getElementById(`${id}`);
-  tarjeta.querySelector(".recipe-title").innerHTML = platillo.nombre;
-  tarjeta.querySelector(".recipe-ingredients").innerHTML = platillo.ingredientes;
-  tarjeta.querySelector(".recipe-price").innerHTML = platillo.precio;
+  tarjeta.querySelector('.recipe-title').innerHTML = platillo.nombre;
+  tarjeta.querySelector('.recipe-ingredients').innerHTML = platillo.ingredientes;
+  tarjeta.querySelector('.recipe-title:last-child').innerHTML = `Precio: $${platillo.precio}`;
 }
+
+
 
 document.querySelector('.recipes').addEventListener('click', function(e) {
   const icono = e.target.closest('.recipe-delete .material-icons');
@@ -59,67 +68,4 @@ document.querySelector('.recipes').addEventListener('click', function(e) {
       console.log(error);
       alert('Error al eliminar el platillo');
     });
-});
-
-
-let streaming = false;
-const width = 100;
-let height = 0;
-
-const video = document.getElementById("video");
-const canvas = document.getElementById("canvas");
-const foto = document.getElementById("foto");
-const btnFoto = document.getElementById('btnFoto');
-const btnCapturar = document.getElementById('tomarFoto');
-
-btnFoto.addEventListener("click", function(e){
-  e.preventDefault();
-  navigator.mediaDevices
-    .getUserMedia({
-      video: {
-        facingMode: { ideal: "environment" }
-      },
-      audio: false
-    })
-    .then((stream) => {
-      video.srcObject = stream;
-      video.play();
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-});
-
-video.addEventListener("canplay", function(){
-  if (!streaming){
-    height = video.videoHeight / (video.videoWidth / width);
-    video.setAttribute("width", width);
-    video.setAttribute("height", height);
-    streaming = true;
-  }
-});
-
-function limpiarFoto(){
-  const contexto = canvas.getContext("2d");
-  contexto.fillStyle = "#AAA";
-  contexto.fillRect(0, 0, canvas.width, canvas.height);
-  foto.setAttribute("src", "");
-}
-
-function capturarFoto(){
-  const contexto = canvas.getContext("2d");
-  if (width && height){
-    canvas.width = width;
-    canvas.height = height;
-    contexto.drawImage(video, 0, 0, width, height);
-    const fotoFinal = canvas.toDataURL("image/png");
-    foto.setAttribute("src", fotoFinal);
-  } else {
-    limpiarFoto();
-  }
-}
-
-btnCapturar.addEventListener("click", function(e){
-  e.preventDefault();
-  capturarFoto();
 });
