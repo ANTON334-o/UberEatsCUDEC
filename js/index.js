b43bd7d0dd8c0f8ba640fa172603d46756ea811c
ANTON334-o <eliseogamer64@gmail.com>
2026-08-12 20:48:57 -0600

    errores

--- js/index.js
+++ js/index.js
@@ -82,6 +82,8 @@ const btnCapturar = document.getElementById('btnCapturar');
 
 btnFoto.addEventListener("click", function(e){
   e.preventDefault();
+  video.style.display = "";
+  foto.setAttribute("src", "");
   navigator.mediaDevices
     .getUserMedia({
       video: {
@@ -122,6 +124,10 @@ function capturarFoto(){
     contexto.drawImage(video, 0, 0, width, height);
     const fotoFinal = canvas.toDataURL("image/png");
     foto.setAttribute("src", fotoFinal);
+    video.style.display = "none";
+    if (video.srcObject) {
+      video.srcObject.getTracks().forEach(function(track){ track.stop(); });
+    }
   } else {
     limpiarFoto();
   }
