79938915a752c47267f8935765d50dfd0f908043
ANTON334-o <eliseogamer64@gmail.com>
2026-08-20 20:53:43 -0600

    nloa mames era una o

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
   const menus = document.querySelectorAll('.side-menu');
   M.Sidenav.init(menus, { edge: 'right' });
 
-o
+
   const forms = document.querySelectorAll('.side-form');
   M.Sidenav.init(forms, { edge: 'left' });
 
